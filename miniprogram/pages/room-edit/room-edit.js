055ce19ba3a69005d246930afc86a1ae67898e13
superylcfly <350009335@qq.com>
2019-11-01 17:03:20 +0800

    图片上传
    图片上传

--- miniprogram/pages/room-edit/room-edit.js
+++ miniprogram/pages/room-edit/room-edit.js
@@ -42,8 +42,8 @@ Page({
   ChooseImage() {
     wx.chooseImage({
       count: 6, //默认9
-      sizeType: ['original', 'compressed'], //可以指定是原图还是压缩图，默认二者都有
-      sourceType: ['album'], //从相册选择
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 从相册选择或者拍照
       success: (res) => {
         if (this.data.imgList.length != 0) {
           this.setData({
@@ -83,8 +83,62 @@ Page({
    * 提交表单
    */
   formSubmit: function(e) {
-    let subRoom = e.detail.value
-    subRoom.provide = this.data.provideArr
-    console.log(subRoom)
+    let submitRoom = e.detail.value
+    submitRoom.provide = this.data.provideArr
+    this.newRoom(submitRoom)
+  },
+
+  /**
+   * 新增房源
+   */
+  newRoom: function(room) {
+    wx.showLoading({
+      title: '正在提交。。。',
+    })
+    let picIds = []
+    // 图片数量
+    let picNu = this.data.imgList.length
+    // 上传成功的数量
+    let successNu = 0
+    this.data.imgList.forEach((item, index) => {
+      // 将图片上传至云存储空间
+      wx.cloud.uploadFile({
+        // 指定上传到的云路径
+        cloudPath: item.substr(item.lastIndexOf('.', item.lastIndexOf('.') - 1) + 1),
+        // 指定要上传的文件的小程序临时文件路径
+        filePath: item,
+        // 成功回调
+        success: res => {
+          picIds.push(res.fileID)
+          successNu++
+          // 全部上传完毕，提交表单
+          if (successNu === picNu) {
+            room.pic = picIds
+            console.log(room)
+          }
+        },
+        fail: e => {
+          picNu--
+        }
+      })
+
+    })
+  },
+
+  /**
+   * 提交房源
+   */
+  submitRoom: function(room) {
+
+    const db = wx.cloud.database()
+    db.collection('room').doc(roomId).get()
+      .then(res => {
+        this.setData({
+          room: res.data
+        })
+      })
+      .then(res => wx.hideLoading())
+      .catch(err => console.error(err))
   }
+
 })
