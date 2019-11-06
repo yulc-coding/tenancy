7bd6779abb0718cd5dab5568b1e7951c4f98b88e
superylcfly <350009335@qq.com>
2019-11-06 16:26:55 +0800

    刷新页面
    下拉刷新，上拉加载

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,4 +1,3 @@
-//index.js
 const app = getApp()
 
 Page({
@@ -7,6 +6,8 @@ Page({
    * 页面的初始数据
    */
   data: {
+    page: 1, // 当前页
+    pageSize: 10, // 每页显示
     roomList: []
   },
 
@@ -14,8 +15,8 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    this.doSearch()
+  onLoad: function(options) {
+    this.doSearch(false)
   },
 
 
@@ -23,8 +24,8 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    * 刷新页面数据
    */
-  onPullDownRefresh: function () {
-    this.doSearch()
+  onPullDownRefresh: function() {
+    this.doSearch(true)
   },
 
 
@@ -32,24 +33,73 @@ Page({
    * 页面上拉触底事件的处理函数
    * 加载更多数据
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
+    wx.showLoading({
+      title: '玩命加载中'
+    })
+    const db = wx.cloud.database()
+    // 获取总条数
+    db.collection('room').where({
+      free: true
+    }).count({
+      success: function(res) {
+        const total = res.total
+        // 总页数
+        const pages = total % this.data.pageSize == 0 ? Math.floor(total % this.data.pageSize) : Math.ceil(total / this.data.pageSize)
+        if (this.page < pages) {
+          db.collection('room')
+            .where({
+              free: true
+            })
+            .skip(this.page * this.pageSize)
+            .limit(this.data.pageSize)
+            .get({
+              success: (res => {
+                this.setData({
+                  roomList: this.data.roomList.concat(res.data),
+                  page: this.page + 1 // 当前页+1
+                })
+              }),
+              fail: (err => {
+                console.error(err)
+              }),
+              complete: () => {
+                wx.hideLoading()
+                console.log(this.data.page)
+              }
+            })
+        }
+      }
+    })
   },
 
 
-  doSearch: function () {
+  doSearch: function(isReload) {
+    wx.showLoading()
     const db = wx.cloud.database()
     db.collection('room')
       .where({
         free: true
       })
-      .get()
-      .then(res => {
-        this.setData({
-          roomList: res.data
-        })
+      .skip(0)
+      .limit(this.data.pageSize)
+      .get({
+        success: (res => {
+          this.setData({
+            roomList: res.data
+          })
+        }),
+        fail: (err => {
+          console.error(err)
+        }),
+        complete: () => {
+          wx.hideLoading()
+          if (isReload) {
+            wx.stopPullDownRefresh() //停止下拉刷新
+          }
+        }
       })
-      .catch(err => console.error(err))
   }
 
 })
