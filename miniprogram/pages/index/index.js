052b987c72b842901eb4c0a2d17948427a012e30
superylcfly <350009335@qq.com>
2019-11-07 09:32:13 +0800

    上拉
    上拉加载调整

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -8,7 +8,10 @@ Page({
   data: {
     page: 1, // 当前页
     pageSize: 10, // 每页显示
-    roomList: []
+    roomList: [],
+    bottomLoad: false, // 是否正在上拉加载更多
+    noMoreList: false, // 已经没有下一页数据了
+    loading: 0 // 当前数据加载百分比
   },
 
 
@@ -28,13 +31,27 @@ Page({
     this.doSearch(true)
   },
 
-
   /**
    * 页面上拉触底事件的处理函数
    * 加载更多数据
    */
   onReachBottom: function() {
-    var that = this
+    console.log(0)
+    // 正在加载中，不处理
+    if (this.data.bottomLoad) {
+      console.log(1)
+      return
+    }
+    // 已经没有数据了，不处理
+    if (this.data.noMoreList) {
+      console.log(2)
+      return
+    }
+    console.log(3)
+    // 设为正在加载中
+    this.setData({
+      bottomLoad: true
+    })
     wx.showLoading({
       title: '玩命加载中'
     })
@@ -57,24 +74,34 @@ Page({
             .limit(this.data.pageSize)
             .get({
               success: (res => {
-                console.log(res.data)
-                that.setData({
-                  roomList: that.data.roomList.concat(res.data),
-                  page: that.data.page + 1 // 当前页+1
+                let loading = Math.ceil((this.data.page + 1) / pages)
+                this.setData({
+                  roomList: this.data.roomList.concat(res.data),
+                  page: this.data.page + 1, // 当前页+1
+                  loading: loading
                 })
               }),
               fail: (err => {
                 console.error(err)
               }),
               complete: () => {
+                // 设为加载结束了
+                this.setData({
+                  bottomLoad: false
+                })
                 wx.hideLoading()
               }
             })
         } else {
+          // 设为记载结束，没有未加载数据了
+          this.setData({
+            bottomLoad: false,
+            noMoreList: true,
+            loading: 100
+          })
           wx.hideLoading()
           console.log("there is no item")
         }
-
       })
   },
 
@@ -91,7 +118,10 @@ Page({
       .get({
         success: (res => {
           this.setData({
-            roomList: res.data
+            roomList: res.data,
+            bottomLoad: false,
+            noMoreList: false,
+            loading: 0
           })
         }),
         fail: (err => {
